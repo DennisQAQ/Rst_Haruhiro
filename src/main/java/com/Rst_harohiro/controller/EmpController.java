6035a0f0a3fe18627f8d9dfd15d7b80b5115d7ae
DearDennisWang <DearDennisWang@gmail.com>
2023-07-04 14:58:58 +0800

    新增员工

--- src/main/java/com/Rst_harohiro/controller/EmpController.java
+++ src/main/java/com/Rst_harohiro/controller/EmpController.java
@@ -5,16 +5,17 @@ import com.Rst_harohiro.entities.Employee;
 import com.Rst_harohiro.service.EmpService;
 import com.Rst_harohiro.service.impl.EmpServiceImpl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/employee")
 public class EmpController {
@@ -69,5 +70,28 @@ public class EmpController {
         //清理Session中保存的当前员工登录的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功！");
+    }
+
+    /**
+     * 添加新员工功能
+     * 页面发送ajax请求，将新增员工页面中输入的数据以json的形式提交到服务端
+     * 服务端Controller接收页面提交的数据并调用Service将数据进行保存
+     * Service调用Mapper操作数据库，保存数据
+     */
+    @PostMapping
+    R<String> seve(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工{}",employee.toString());
+        //设置初始密码
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //获取当前创建人的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        empService.save(employee);
+        return R.success("新增员工成功！");
+
     }
 }
